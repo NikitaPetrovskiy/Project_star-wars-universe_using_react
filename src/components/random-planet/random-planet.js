3150a0daba0f676efae0886410002a3c330615aa
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-10-11 15:22:28 +0300

    Created functions fro transforming data object into 'SwapiService' and added logic to get planet to 'RandomPlanet' component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapiService';
 
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+    swapiService = new SwapiService();
+    state = {
+        planet: {}
+    };
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+    onPlanetLoaded = (planet) => {
+      this.setState({ planet });
+    };
+    updatePlanet() {
+        const id = Math.floor(Math.random() * 25) + 2;
+        this.swapiService
+          .getPlanet(id)
+          .then( this.onPlanetLoaded );
+    };
+
     render() {
+        const { planet : { id, name, population,
+            rotationPeriod, diameter } } = this.state;
+
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/7.jpg`} />
+                     src={`https://starwars-visualguide.com/assets/img/planets/${ id }.jpg`} />
                 <div>
-                    <h4>Planet Name</h4>
+                    <h4>{ name }</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
                             <span className="term">Population</span>
-                            <span>population</span>
+                            <span>{ population }</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Rotation Period</span>
-                            <span>rotation period</span>
+                            <span>{ rotationPeriod }</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Diameter</span>
-                            <span>diameter</span>
+                            <span>{ diameter }</span>
                         </li>
                     </ul>
                 </div>
             </div>
-
         );
     }
-}
+};
