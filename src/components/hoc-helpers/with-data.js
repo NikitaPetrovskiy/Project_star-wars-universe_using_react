76bf39a4a5ff5211933b10e9ea8862d2f0ad2af8
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-10-24 15:06:09 +0300

    Updated 'with-data' hoc, added properties loading and error to state

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Spinner from "../spinner"
+import ErrorIndicator from "../error-indicator";
 
 const withData = (View) => {
     return class  extends Component {
         state = {
-            data: null
+            data: null,
+            loading: true,
+            error: false
         };
         componentDidMount() {
             this.updateData();
@@ -16,18 +19,32 @@ const withData = (View) => {
         };
 
         updateData() {
+            this.setState({
+                loading: true,
+                error: false
+            })
             this.props.getData()
                 .then((data) => {
                     this.setState({
-                        data
+                        data,
+                        loading: false
+                    });
+                })
+                .catch(() => {
+                    this.setState({
+                        error: true,
+                        loading: false
                     });
                 });
         };
 
         render() {
-            const { data } = this.state;
-            if (!data) {
+            const { data, loading, error } = this.state;
+            if (loading) {
                 return <Spinner />
+            }
+            if (error) {
+                return <ErrorIndicator />
             }
             return <View {...this.props} data={data} />
         };
