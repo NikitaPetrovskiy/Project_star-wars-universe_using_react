4319bcf447a2a1d7cc3fffba530ea1c1f0b35d95
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-10-25 14:36:39 +0300

    Added library 'prop-types' and used defaulProps and PropTypes for cheking properties

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './item-list.css';
 
@@ -21,6 +22,15 @@ const ItemList = (props) => {
             {items}
         </ul>
     );
+};
+
+ItemList.defaulProps = {
+  onItemSelected: () => {}
+};
+ItemList.propTypes = {
+    onItemSelected: PropTypes.func,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    children: PropTypes.func.isRequired
 };
 
 export default ItemList;
